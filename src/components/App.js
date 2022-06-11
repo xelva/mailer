469653c329b00e56b0511699dea87ec457ded25a
xelva <xelvacodes@gmail.com>
2022-06-11 13:15:54 -0500

    client

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,6 @@ import { Outlet } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 import Header from './Header';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-
-const stripePromise = loadStripe('pk_test_51L9DUDEIwvXoAgPncCCQoMOoLS4X5vMzhERCc4zibrhhbaijdiIdZQnV4hIKHYlkwdgqyjyvccVKOjYUfJYqixZH00MDN2Ji96')
-
 
 const App = ({ fetchUser }) => {
     useEffect(() => {
@@ -17,14 +12,15 @@ const App = ({ fetchUser }) => {
 
     return (
         <div className="container">
-            <Elements stripe={stripePromise}>
+            
                 <div>
                     <Header />
                 </div>
                 <div>
                     <Outlet />
                 </div>
-            </Elements>
+                lalala
+            
         </div>
         
     )
